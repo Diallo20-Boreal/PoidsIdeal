7acd5c142e820c49f313c70e5bf67cb858a9bc4c
ALHASSANE DIALLO <alhassane.diallo@monboreal.ca>
2024-11-27 15:23:15 -0500

    Poids version 4.0

--- MauiAppPoidsIdeal/MainPage.xaml.cs
+++ MauiAppPoidsIdeal/MainPage.xaml.cs
@@ -12,11 +12,16 @@
         private void femmebtn(object sender, EventArgs e)
         {
 
+            double taille = double.Parse(TaillefEntry.Text);
+            double poids = taille - 100 - (taille - 150)/2.5;
+            PoidsfLabel.Text = $"{poids} kg";
         }
 
         private void hommebtn(object sender, EventArgs e)
         {
-
+            double taille = double.Parse(TaillehEntry.Text);
+            double poids = taille - 100 - (taille - 150)/4;
+            PoidshLabel.Text = $"{poids} kg";
         }
     }
 }
