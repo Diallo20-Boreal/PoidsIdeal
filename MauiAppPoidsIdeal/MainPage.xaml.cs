fab858ee0c01fc9f2623dd50738c4f59a9e6ee54
ALHASSANE DIALLO <alhassane.diallo@monboreal.ca>
2024-11-27 15:03:50 -0500

    Poids version 3.0

--- MauiAppPoidsIdeal/MainPage.xaml.cs
+++ MauiAppPoidsIdeal/MainPage.xaml.cs
@@ -9,5 +9,14 @@
             InitializeComponent();
         }
 
+        private void femmebtn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hommebtn(object sender, EventArgs e)
+        {
+
+        }
     }
 }
